d922a06821168920624aa91e5c9bab5a89591a30
Thomas pradeep <60232180+Thomaspradeep@users.noreply.github.com>
2023-10-23 13:43:48 +0530

    Update sampletest.py

--- sampletest.py
+++ sampletest.py
@@ -1,7 +1,4 @@
-# Python 3 program for recursive binary search.
-# Modifications needed for the older Python 2 are found in comments.
 
-# Returns index of x in arr if present, else -1
 name = "thomas"
 def binary_search(arr, low, high, x):
 
@@ -10,21 +7,16 @@ def binary_search(arr, low, high, x):
 
 		mid = (high + low) // 2
 
-		# If element is present at the middle itself
 		if arr[mid] == x:
 			return mid
 
-		# If element is smaller than mid, then it can only
-		# be present in left subarray
 		elif arr[mid] > x:
 			return binary_search(arr, low, mid - 1, x)
 
-		# Else the element can only be present in right subarray
 		else:
 			return binary_search(arr, mid + 1, high, x)
 
 	else:
-		# Element is not present in the array
 		return -1
 
 # Test array
@@ -34,7 +26,7 @@ x = 10
 arr = [ 2, 3, 4, 10, 40 ]
 x = 10
 
-# Function call
+
 result = binary_search(arr, 0, len(arr)-1, x)
 
 if result != -1:
