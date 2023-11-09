6012d72f313d7d3caaa582c820c83a6b52e0f794
Thomas pradeep <60232180+Thomaspradeep@users.noreply.github.com>
2023-11-09 14:47:04 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,10 +1,11 @@
 def sum(number1,number2):
     result= number1+number2
     return result
-
+    
 def sub(number1,number2):
     result= number1-number2
     return result
+    
 def mul(number1,number2):
     result= number1*number2
     return result
